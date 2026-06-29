1a273bffa8ac83123c8b34ef376f934ad85143a6
NoBitches6420 <hackerato38@gmail.com>
2026-06-29 20:04:55 +0200

    Create BDAFR.lua

---
+++ src/games/bedwars/6872274481 - game/Blatant/BDAFR.lua
@@ -0,0 +1,18 @@
+local root = game.Players.LocalPlayer.Character.Head
+local http = game:GetService('HttpService')
+
+local args = {
+    game:GetService("ReplicatedStorage"):WaitForChild("Inventories"):WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("snowball"), 
+    "snowball",
+    "meteor_shower",
+    root.CFrame.Position,
+    root.CFrame.Position,
+    Vector3.new(0, 5, 0),
+    http:GenerateGUID(false),
+    {
+        shotId = http:GenerateGUID(false),
+        drawDurationSec = 1
+    },
+    workspace:GetServerTimeNow(),
+}
+game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("ProjectileFire"):InvokeServer(unpack(args))
