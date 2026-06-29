d034241bb382d464bb69a76a1c37dabf3b857e4f
NoBitches6420 <hackerato38@gmail.com>
2026-06-29 21:42:47 +0200

    Update BDAFR.lua

--- src/games/bedwars/6872274481 - game/Blatant/BDAFR.lua
+++ src/games/bedwars/6872274481 - game/Blatant/BDAFR.lua
@@ -1,24 +1,39 @@
 
 run(function()
     local BDAFR 
+    local Option
     local root = game.Players.LocalPlayer.Character.Head
     local http = cloneref(game:GetService('HttpService'))
 
 
-    vape
-    local args = {
-        game:GetService("ReplicatedStorage"):WaitForChild("Inventories"):WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("snowball"), 
-        "snowball",
-        "meteor_shower",
-        root.CFrame.Position,
-        root.CFrame.Position,
-        Vector3.new(0, 5, 0),
-        http:GenerateGUID(false),
-        {
-            shotId = http:GenerateGUID(false),
-            drawDurationSec = 1
-        },
-        workspace:GetServerTimeNow(),
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("ProjectileFire"):InvokeServer(unpack(args))
+    BDAFR = vape.Catagories.Blatant:CreateModule({
+        Name = "BDAFR"
+        Function = function()
+            while task.wait() do
+                local args = {
+                    game:GetService("ReplicatedStorage"):WaitForChild("Inventories"):WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("snowball"), 
+                    "snowball",
+                    Option,
+                    root.CFrame.Position,
+                    root.CFrame.Position,
+                    Vector3.new(0, 5, 0),
+                    http:GenerateGUID(false),
+                    {
+                        shotId = http:GenerateGUID(false),
+                        drawDurationSec = 1
+                    },
+                    workspace:GetServerTimeNow(),
+                }
+                game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("ProjectileFire"):InvokeServer(unpack(args))
+            end
+        end,
+        ToolTip = "BDAFR exploit v69"
+    })
+    BDAFR:CreateDropdown({
+        Name = "Projectile Mode"
+        List = ReplicatedStorage.Assets.Projectiles:GetChildren()
+        Function = function(Projectile)
+            Option = Projectile
+        end,
+    })
 end
